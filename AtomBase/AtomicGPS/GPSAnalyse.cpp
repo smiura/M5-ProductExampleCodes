707938a1d793c562436587f00f22acde7ca11bda
Satoshi Miura <smiura@users.noreply.github.com>
2020-12-28 00:23:08 +0900

    Fix NMEA 0183 sentence analyse
    Exclude talker from statement identifiers
    ex:
    $--RMC...
    $--GSV,,,

--- AtomBase/AtomicGPS/GPSAnalyse.cpp
+++ AtomBase/AtomicGPS/GPSAnalyse.cpp
@@ -44,15 +44,15 @@ void GPSAnalyse::run(void *data)
 void GPSAnalyse::upDate()
 {
     xSemaphoreTake(_xSemaphore, portMAX_DELAY);
-    memcpy(&s_GNRMC,&_s_GNRMC,sizeof(GNRMC_t));
-    memcpy(&s_GNGAS,&_s_GNGAS,sizeof(GNGSA_t));
-    memcpy(&s_GPGSV,&_s_GPGSV,sizeof(GPGSV_t));
+    memcpy(&s_RMC,&_s_RMC,sizeof(RMC_t));
+    memcpy(&s_GAS,&_s_GAS,sizeof(GSA_t));
+    memcpy(&s_GSV,&_s_GSV,sizeof(GSV_t));
     xSemaphoreGive(_xSemaphore);
 }
 
-void GPSAnalyse::AnaGPRMC(String str)
+void GPSAnalyse::AnaRMC(String str)
 {
-    str = str.substring(str.indexOf("GNRMC"), str.length());
+    str = str.substring(str.indexOf("RMC"), str.length());
     if (str.indexOf('*') != -1)
     {
         int indexsub = str.indexOf('*');
@@ -78,44 +78,44 @@ void GPSAnalyse::AnaGPRMC(String str)
                 Serial.printf("%d:", pamindex);
                 Serial.println(pamstr);
 #endif
-                _s_GNRMC.pamstr[pamindex] = pamstr;
+                _s_RMC.pamstr[pamindex] = pamstr;
             }
             else
             {
-                _s_GNRMC.pamstr[pamindex].clear();
+                _s_RMC.pamstr[pamindex].clear();
             }
 
             pamindex++;
         }
     }
 
-    _s_GNRMC.Utc = _s_GNRMC.pamstr[1];
-    if (_s_GNRMC.pamstr[2].length() != 0)
+    _s_RMC.Utc = _s_RMC.pamstr[1];
+    if (_s_RMC.pamstr[2].length() != 0)
     {
-        _s_GNRMC.State = _s_GNRMC.pamstr[2].charAt(0);
+        _s_RMC.State = _s_RMC.pamstr[2].charAt(0);
     }
 
     {
-        _s_GNRMC.Latitude = _s_GNRMC.pamstr[3].substring(0, 2).toDouble() + _s_GNRMC.pamstr[3].substring(2, 4).toDouble() / 60 + _s_GNRMC.pamstr[3].substring(5, 9).toDouble() / 600000;
+        _s_RMC.Latitude = _s_RMC.pamstr[3].substring(0, 2).toDouble() + _s_RMC.pamstr[3].substring(2, 4).toDouble() / 60 + _s_RMC.pamstr[3].substring(5, 9).toDouble() / 600000;
     }
-    //_s_GNRMC.Latitude        = _s_GNRMC.pamstr[3].toDouble();
-    _s_GNRMC.LatitudeMark = _s_GNRMC.pamstr[4].charAt(0);
+    //_s_RMC.Latitude        = _s_RMC.pamstr[3].toDouble();
+    _s_RMC.LatitudeMark = _s_RMC.pamstr[4].charAt(0);
     {
-        _s_GNRMC.Longitude = _s_GNRMC.pamstr[5].substring(0, 3).toDouble() + _s_GNRMC.pamstr[5].substring(3, 5).toDouble() / 60 + _s_GNRMC.pamstr[5].substring(6, 10).toDouble() / 600000;
+        _s_RMC.Longitude = _s_RMC.pamstr[5].substring(0, 3).toDouble() + _s_RMC.pamstr[5].substring(3, 5).toDouble() / 60 + _s_RMC.pamstr[5].substring(6, 10).toDouble() / 600000;
     }
-    //_s_GNRMC.Longitude       = _s_GNRMC.pamstr[5].toDouble();
-    _s_GNRMC.LongitudeMark = _s_GNRMC.pamstr[6].charAt(0);
-    _s_GNRMC.TrackSpeed = _s_GNRMC.pamstr[7].toFloat();
-    _s_GNRMC.TrackAngle = _s_GNRMC.pamstr[8].toFloat();
-    _s_GNRMC.Date = _s_GNRMC.pamstr[9];
-    _s_GNRMC.Magnetic = _s_GNRMC.pamstr[10].toFloat();
-    _s_GNRMC.Declination = _s_GNRMC.pamstr[11].charAt(0);
-    _s_GNRMC.mode = _s_GNRMC.pamstr[12].charAt(0);
+    //_s_RMC.Longitude       = _s_RMC.pamstr[5].toDouble();
+    _s_RMC.LongitudeMark = _s_RMC.pamstr[6].charAt(0);
+    _s_RMC.TrackSpeed = _s_RMC.pamstr[7].toFloat();
+    _s_RMC.TrackAngle = _s_RMC.pamstr[8].toFloat();
+    _s_RMC.Date = _s_RMC.pamstr[9];
+    _s_RMC.Magnetic = _s_RMC.pamstr[10].toFloat();
+    _s_RMC.Declination = _s_RMC.pamstr[11].charAt(0);
+    _s_RMC.mode = _s_RMC.pamstr[12].charAt(0);
 }
 
-void GPSAnalyse::AnaGNGAS(String str)
+void GPSAnalyse::AnaGAS(String str)
 {
-    str = str.substring(str.indexOf("GNGSA"), str.length());
+    str = str.substring(str.indexOf("GSA"), str.length());
 
     if (str.indexOf('*') != -1)
     {
@@ -140,31 +140,31 @@ void GPSAnalyse::AnaGNGAS(String str)
                 Serial.printf("%d:", pamindex);
                 Serial.println(pamstr);
 #endif
-                _s_GNGAS.pamstr[pamindex] = pamstr;
+                _s_GAS.pamstr[pamindex] = pamstr;
             }
             else
             {
-                _s_GNGAS.pamstr[pamindex].clear();
+                _s_GAS.pamstr[pamindex].clear();
             }
             pamindex++;
         }
     }
 
-    _s_GNGAS.mode2 = _s_GNGAS.pamstr[1].charAt(0);
-    _s_GNGAS.mode1 = _s_GNGAS.pamstr[2].toInt();
+    _s_GAS.mode2 = _s_GAS.pamstr[1].charAt(0);
+    _s_GAS.mode1 = _s_GAS.pamstr[2].toInt();
     for (size_t i = 0; i < 12; i++)
     {
-        _s_GNGAS.PINMap[i] = _s_GNGAS.pamstr[3 + i].toInt();
+        _s_GAS.PINMap[i] = _s_GAS.pamstr[3 + i].toInt();
     }
-    _s_GNGAS.PDOP = _s_GNGAS.pamstr[15].toFloat();
-    _s_GNGAS.HDOP = _s_GNGAS.pamstr[16].toFloat();
-    _s_GNGAS.VDOP = _s_GNGAS.pamstr[17].toFloat();
+    _s_GAS.PDOP = _s_GAS.pamstr[15].toFloat();
+    _s_GAS.HDOP = _s_GAS.pamstr[16].toFloat();
+    _s_GAS.VDOP = _s_GAS.pamstr[17].toFloat();
 }
 
-void GPSAnalyse::AnaGPGSV(String str)
+void GPSAnalyse::AnaGSV(String str)
 {
     //Serial.println(str);
-    str = str.substring(str.indexOf("GPGSV"), str.length());
+    str = str.substring(str.indexOf("GSV"), str.length());
 
     if (str.indexOf('*') != -1)
     {
@@ -189,11 +189,11 @@ void GPSAnalyse::AnaGPGSV(String str)
                 Serial.printf("%d:", pamindex);
                 Serial.println(pamstr);
 #endif
-                _s_GPGSV.pamstr[pamindex] = pamstr;
+                _s_GSV.pamstr[pamindex] = pamstr;
             }
             else
             {
-                _s_GPGSV.pamstr[pamindex].clear();
+                _s_GSV.pamstr[pamindex].clear();
             }
             pamindex++;
         }
@@ -201,38 +201,38 @@ void GPSAnalyse::AnaGPGSV(String str)
     int SatelliteSize = (pamindex - 4) / 4;
     //Serial.printf("Number%d\n", SatelliteSize);
 
-    _s_GPGSV.size = _s_GPGSV.pamstr[1].toInt();
-    _s_GPGSV.Number = _s_GPGSV.pamstr[2].toInt();
-    _s_GPGSV.SatelliteSize = _s_GPGSV.pamstr[3].toInt();
+    _s_GSV.size = _s_GSV.pamstr[1].toInt();
+    _s_GSV.Number = _s_GSV.pamstr[2].toInt();
+    _s_GSV.SatelliteSize = _s_GSV.pamstr[3].toInt();
 
-    if (_s_GPGSV.Number == 1)
+    if (_s_GSV.Number == 1)
     {
         for (size_t i = 0; i < 32; i++)
         {
-            _s_GPGSV.Satellite[i].flag = false;
+            _s_GSV.Satellite[i].flag = false;
         }
     }
     for (size_t i = 0; i < SatelliteSize; i++)
     {
-        int id = _s_GPGSV.pamstr[4 + (i * 4) + 0].toInt();
+        int id = _s_GSV.pamstr[4 + (i * 4) + 0].toInt();
         if( id >= 32 ) continue;
         if(( 7 + (i * 4)) > 50 )
         {
             break;
         }
-        _s_GPGSV.Satellite[id].elevation = _s_GPGSV.pamstr[4 + (i * 4) + 1].toInt();
-        _s_GPGSV.Satellite[id].Azimuth = _s_GPGSV.pamstr[4 + (i * 4) + 2].toInt();
-        _s_GPGSV.Satellite[id].SNR = (_s_GPGSV.pamstr[4 + (i * 4) + 3].length() == 0) ? -1 : _s_GPGSV.pamstr[4 + (i * 4) + 3].toInt();
-        _s_GPGSV.Satellite[id].flag = true;
+        _s_GSV.Satellite[id].elevation = _s_GSV.pamstr[4 + (i * 4) + 1].toInt();
+        _s_GSV.Satellite[id].Azimuth = _s_GSV.pamstr[4 + (i * 4) + 2].toInt();
+        _s_GSV.Satellite[id].SNR = (_s_GSV.pamstr[4 + (i * 4) + 3].length() == 0) ? -1 : _s_GSV.pamstr[4 + (i * 4) + 3].toInt();
+        _s_GSV.Satellite[id].flag = true;
     }
 
-    if (_s_GPGSV.Number == _s_GPGSV.size)
+    if (_s_GSV.Number == _s_GSV.size)
     {
         for (size_t i = 0; i < 32; i++)
         {
-            if (_s_GPGSV.Satellite[i].flag == true)
+            if (_s_GSV.Satellite[i].flag == true)
             {
-                Serial.printf("ID %d:%d,%d,%d\n", i, _s_GPGSV.Satellite[i].elevation, _s_GPGSV.Satellite[i].Azimuth, _s_GPGSV.Satellite[i].SNR);
+                Serial.printf("ID %d:%d,%d,%d\n", i, _s_GSV.Satellite[i].elevation, _s_GSV.Satellite[i].Azimuth, _s_GSV.Satellite[i].SNR);
             }
         }
     }
@@ -244,32 +244,32 @@ void GPSAnalyse::Analyse()
     {
         int index = _GPS_Str.indexOf('\r');
         String str = _GPS_Str.substring(0, index);
-        _GPS_Str = _GPS_Str.substring(index + 3, _GPS_Str.length());
+        _GPS_Str = _GPS_Str.substring(index + 5, _GPS_Str.length());
 
         str.trim();
 
         //Serial.println(str);
 
-        if (str.indexOf("GNRMC") != -1)
+        if (str.indexOf("RMC") != -1)
         {
-            AnaGPRMC(str);
+            AnaRMC(str);
         }
-        else if (str.indexOf("GPVTG") != -1)
+        else if (str.indexOf("VTG") != -1)
         {
         }
-        else if (str.indexOf("GPGGA") != -1)
+        else if (str.indexOf("GGA") != -1)
         {
 
         }
-        else if (str.indexOf("GNGSA") != -1)
+        else if (str.indexOf("GSA") != -1)
         {
-            //Serial.print("GNGSA:");
-            //AnaGNGAS(str);
+            //Serial.print("GSA:");
+            //AnaGAS(str);
         }
-        else if (str.indexOf("GPGSV") != -1)
+        else if (str.indexOf("GSV") != -1)
         {
             //Serial.print("GPGSV:");
-            AnaGPGSV(str);
+            AnaGSV(str);
         }
         else if (str.indexOf("GPGLL") != -1)
         {
